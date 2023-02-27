7f5f847084486b249418b4b732a440d02046643a
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-02-27 12:57:29 +0100

    created Calculator class

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public void someMethod() {
+    }
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+
+
+    }
+}
