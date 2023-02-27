d6d2ab0a4f0debe0a51f7fb4e329478c14f0f01a
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-02-27 13:05:59 +0100

    add Print to method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public void someMethod() {
+        System.out.println("It works!");
     }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
