edaa5099264fb6d05cb593479c6a9083e2d0c993
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-03-15 18:18:37 +0100

    7.3 Exercises in class StreamMain with collectors.toMap()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,7 +4,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
 
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -12,12 +12,14 @@ public class StreamMain {
 
     public static void main(String[] args) {
         BookDirectory theBookDirectory = new BookDirectory();
-        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toList());
 
-        System.out.println("# elements: " + theResultListOfBooks.size());
-        theResultListOfBooks.stream()
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
 
     }
--- kodilla-stream/src/main/java/com/kodilla/stream/book/Book.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/book/Book.java
@@ -5,11 +5,14 @@ public final class Book {
     private final String author;
     private final String title;
     private final int yearOfPublication;
+    private final String signature;
 
-    public Book(final String author, final String title, final int yearOfPublication) {
+
+    public Book(final String author, final String title, final int yearOfPublication, final String signature) {
         this.author = author;
         this.title = title;
         this.yearOfPublication = yearOfPublication;
+        this.signature = signature;
     }
 
     public String getAuthor() {
@@ -24,12 +27,17 @@ public final class Book {
         return yearOfPublication;
     }
 
+    public String getSignature() {
+        return signature;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
                 "author='" + author + '\'' +
                 ", title='" + title + '\'' +
                 ", yearOfPublication=" + yearOfPublication +
+                ", signature='" + signature + '\'' +
                 '}';
     }
 }
