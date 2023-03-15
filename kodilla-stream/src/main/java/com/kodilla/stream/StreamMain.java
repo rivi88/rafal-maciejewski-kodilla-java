62fa2ed5f5966473d2f2f1f9b56d69efa5dc3eb5
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-03-15 17:05:39 +0100

    7.2 Practice with operation .map

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,35 +1,13 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
-
+import com.kodilla.stream.person.People;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println(poemBeautifier.beautify("Cześć", text -> text + " ABC " + text));
-        System.out.println(poemBeautifier.beautify("hi i'am bot", text -> text.toUpperCase()));
-        System.out.println(poemBeautifier.beautify("Cześć", text -> "<<<" + text + ">>>"));
-        System.out.println(poemBeautifier.beautify("mamy zwiadowce", text -> " <" + text.toUpperCase() + "> " + text));
-        System.out.println(poemBeautifier.beautify("bot", text -> text + " to " + text));
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .forEach(s -> System.out.println(s));
     }
 }
