3c66c2ba2623e6242ef8436827ab4023ab307e89
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-03-15 13:54:38 +0100

    7.1 Text enhancement task

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,15 +1,29 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.Processor;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        Executor coreToExecute = () -> System.out.println("This is an example text.");
-        processor.execute(coreToExecute);
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println(poemBeautifier.beautify("Cześć", text -> text + " ABC " + text));
+        System.out.println(poemBeautifier.beautify("hi i'am bot", text -> text.toUpperCase()));
+        System.out.println(poemBeautifier.beautify("Cześć", text -> "<<<" + text + ">>>"));
     }
 }
