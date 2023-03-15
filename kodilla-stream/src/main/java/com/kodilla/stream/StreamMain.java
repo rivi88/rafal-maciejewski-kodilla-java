e26188bc788fe67760c2129e4e551d6d49d3bf46
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-03-15 17:59:12 +0100

    7.3 Modify StreamMain collectors.toList()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,15 +1,24 @@
 package com.kodilla.stream;
 
 
+import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class StreamMain {
 
     public static void main(String[] args) {
         BookDirectory theBookDirectory = new BookDirectory();
-        theBookDirectory.getList().stream()
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
                 .forEach(System.out::println);
+
     }
 }
