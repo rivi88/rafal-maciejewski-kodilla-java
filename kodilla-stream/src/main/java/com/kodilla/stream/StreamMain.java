183a2cd4be2ba1b81e0554d3de8dd3e39f5d418e
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-03-15 17:36:01 +0100

    7.3 Cascade filtering

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,7 +7,10 @@ public class StreamMain {
 
     public static void main(String[] args) {
         People.getList().stream()
+                .map(String::toUpperCase)
                 .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
                 .forEach(System.out::println);
     }
 }
