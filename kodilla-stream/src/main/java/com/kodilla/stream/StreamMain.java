90f34dccc2742e39def838aa84570a0fbec5f9e3
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-03-15 17:50:03 +0100

    7.3 Create Book, BookDirectory and modify StreamMain

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,15 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.person.People;
+import com.kodilla.stream.book.BookDirectory;
+
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        People.getList().stream()
-                .map(String::toUpperCase)
-                .filter(s -> s.length() > 11)
-                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
-                .filter(s -> s.substring(0, 1).equals("M"))
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
                 .forEach(System.out::println);
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/book/Book.java
@@ -0,0 +1,35 @@
+package com.kodilla.stream.book;
+
+public final class Book {
+
+    private final String author;
+    private final String title;
+    private final int yearOfPublication;
+
+    public Book(final String author, final String title, final int yearOfPublication) {
+        this.author = author;
+        this.title = title;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", yearOfPublication=" + yearOfPublication +
+                '}';
+    }
+}
