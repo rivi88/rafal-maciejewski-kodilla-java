fb25a3feff8c8d7104e62e1f99a479cd0f6c6d7f
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-05-04 23:50:22 +0200

    13.1 Exercise

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        String result = movieStore.getMovies().values().stream()
+                .flatMap(movieTitles -> movieTitles.stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(result);
+    }
+}
