932552448fa5d5fe166673c38c2060ff87333cf5
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-05-06 00:22:41 +0200

    13.2 Exercise

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/exercise/CarRentalService.java
@@ -0,0 +1,14 @@
+package com.kodilla.good.patterns.exercise;
+
+import java.time.LocalDateTime;
+
+public class CarRentalService {
+
+    public boolean rent(final User user, final LocalDateTime carRentFrom, final LocalDateTime carRentTo) {
+        System.out.println("Renting Car for: " + user.getName() + user.getSurname()
+                + " from: " + carRentFrom.toString() + " to: " + carRentTo.toString());
+
+        return true;
+    }
+
+}
