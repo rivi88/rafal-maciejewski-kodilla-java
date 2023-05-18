9840fefcec8643db08c33c20e82c335891640b61
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-05-18 10:30:21 +0200

    13.4 Food2Door

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodShop/Food2Door.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodShop/Food2Door.java
@@ -5,15 +5,12 @@ public class Food2Door {
     public static void main(String[] args) {
         OrderProcessor orderProcessor = new OrderProcessor();
 
-        // Przykładowe zamówienie dla ExtraFoodShop
         orderProcessor.setStrategy(new ExtraFoodShopStrategy());
         orderProcessor.processOrder("ExtraFoodShop", 10, "Mleko");
 
-        // Przykładowe zamówienie dla HealthyShop
         orderProcessor.setStrategy(new HealthyShopStrategy());
         orderProcessor.processOrder("HealthyShop", 5, "Warzywa");
 
-        // Przykładowe zamówienie dla GlutenFreeShop
         orderProcessor.setStrategy(new GlutenFreeShopStrategy());
         orderProcessor.processOrder("GlutenFreeShop", 3, "Chleb");
     }
