9e59f7f8d2ba4fd5c7311daa3558f0bfcbb314cf
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-05-24 15:16:37 +0200

    14.1 exercise

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/SpringIntroRunner.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/SpringIntroRunner.java
@@ -1,4 +1,7 @@
 package com.kodilla.spring.intro;
 
 public class SpringIntroRunner {
+    public static void main(String[] args) {
+        System.out.println("welcome into the ingtro to spring");
+    }
 }
