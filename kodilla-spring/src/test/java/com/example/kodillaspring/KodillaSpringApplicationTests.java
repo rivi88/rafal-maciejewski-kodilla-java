5ce01da02afde847775d1933387dfadd316701c7
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-05-25 13:27:57 +0200

    14.1 exercise

---
+++ kodilla-spring/src/test/java/com/example/kodillaspring/KodillaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
