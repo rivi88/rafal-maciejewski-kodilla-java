d38fbed1b83ca0af44c516ee92a8ad5d9fd3d878
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-05-26 14:41:45 +0200

    14.1 exercise

--- kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
@@ -1,4 +1,4 @@
-package com.example.kodillaspring;
+package com.kodilla.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
