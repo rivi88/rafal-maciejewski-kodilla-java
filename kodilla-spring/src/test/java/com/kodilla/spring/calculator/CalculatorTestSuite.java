797257f30f234f671669b154fb1ac451aca65dbf
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-05-31 23:57:32 +0200

    14.2 Task

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CalculatorTestSuite {
-
+    //todo @Autowired
     @Test
     public void testCalculations() {
         Calculator calculator = new Calculator(new Display());
