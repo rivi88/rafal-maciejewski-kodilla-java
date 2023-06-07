b7dfbd74828c619ba1aa320a127ef3fc1b036478
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-06-07 11:42:14 +0200

    14.2 exercise

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -10,12 +10,9 @@ import java.util.List;
 public final class Library {
 
     private final List<String> books = new ArrayList<>();
-    private LibraryDbController libraryDbController;
 
     @Autowired
-    public void setLibraryDbController(LibraryDbController libraryDbController) {
-        this.libraryDbController = libraryDbController;
-    }
+    private LibraryDbController libraryDbController;
 
     public void saveToDb() {
         libraryDbController.saveData();
