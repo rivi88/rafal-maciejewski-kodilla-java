2199f3fd5f1702c76f42d1a7872889c4eb65123f
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-06-13 20:08:07 +0200

    17.1 task

---
+++ kodilla-hibernate/src/main/java/com/example/kodilla/hibernate/KodillaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodilla.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
+
+}
---
+++ kodilla-hibernate/src/test/java/com/example/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodilla.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
