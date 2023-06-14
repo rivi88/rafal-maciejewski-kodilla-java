6221af4274da063b77ea3970a4ce157b99f728b8
Rafał Maciejewski <rvmaciejewski@gmail.com>
2023-06-14 18:02:40 +0200

    17.1 exercise

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,4 +1,4 @@
-package com.example.kodilla.hibernate;
+package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class KodillaHibernateApplication {
     public static void main(String[] args) {
         SpringApplication.run(KodillaHibernateApplication.class, args);
     }
-
 }
